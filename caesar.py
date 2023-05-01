5549072c01419858ca112ce8ad0be58fedab8c93
do-racoon <kkang58650@gmail.com>
2023-05-01 17:14:04 +0900

    fix : Fix the caesar.py to decode and encode
    to decide decode and encode what you want

--- caesar.py
+++ caesar.py
@@ -1,19 +1,36 @@
-def change_words(str):
+def encode_words(str): ## 암호화 하는 코드
 	sentence = ""
 	for i in str:
-		if ord(i) >= 65 and ord(i) <= 67:
+		if ord(i) >= 65 and ord(i) <= 67: ## 소문자 abc 아스키코드일 때만 xyz로 바꿔줌
 			i = chr(ord(i) + 23)
-			sentence += i
-		elif ord(i) >= 97 and ord(i) <= 99:
+		elif ord(i) >= 97 and ord(i) <= 99: ## 대문자 ABC 아스키코드일 때만 XYZ로 바꿔줌
 			i = chr(ord(i) + 23)
-			sentence += i
-		elif ord(i) <= 65:
-			sentence += i
-		else :
+		elif ord(i) <= 64 or ord(i) >= 123: ## 띄어쓰기포함 특수문자는 패스 
+			pass
+		else :				## 나머지는 3칸씩 앞으로 당김
 			i = chr(ord(i) -3)
-			sentence += i
 
+		sentence += i
 	return sentence
 
-
-print(change_words(input("문장을 적어보세요")))
+def decode_words(str):
+	sentence = ""
+	for i in str:
+		if ord(i) >=120 and ord(i) <= 122: ## 소문자 xyz 아스키코드일 때만 abc로 바꿔줌
+			i = chr(ord(i) - 23)
+		elif ord(i) >= 88 and ord(i) <=90: ## 대문자 XYZ 아스키코드일 때만 ABC로 바꿔줌
+			i = chr(ord(i) -23)
+		elif ord(i) <= 64 or ord(i) >= 123:		## 띄어쓰기포함 특수문자는 패스
+			pass
+		else:				## 나머지는 3칸씩 뒤로 당김
+			i = chr(ord(i) +3)
+		sentence += i
+	return sentence
+def what_mode(mode, str):
+	if mode == "decode":
+		print(decode_words(str))
+	elif mode == "encode":
+		print(encode_words(str))
+	else :
+		print("sorry. we can't do that")
+what_mode(input("모드를 선택하세요"),input("문장을 적어보세요"))
